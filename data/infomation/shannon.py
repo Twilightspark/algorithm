b7dcbee0d7732f3c869ce5d3a1e6b0a20eec361e
Kyle Twilight <waitingfuture@163.com>
2019-12-12 17:23:07 +0800

    决策树开启

---
+++ data/infomation/shannon.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: Zhang Kai
+# Email : zhangkai7@sgepri.sgcc.com.cn
+# Time  : 2019/12/11 10:46
+# Function: (功能介绍)
---
+++ machinelearning/decision/decisiontree.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Author: Zhang Kai
+# Email : zhangkai7@sgepri.sgcc.com.cn
+# Time  : 2019/12/11 11:01
+# Function: (功能介绍)
